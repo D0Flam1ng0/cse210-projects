03bf6664b129d26022b6b05d9a38a22030a7132e
D0Flam1ng0 <and24112@byui.edu>
2025-04-26 10:42:57 -0600

    Parts 1-2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What's your frist name? ");
+        string fristName = Console.ReadLine();
+        Console.Write("What's your last name? ");
+        string LastName = Console.ReadLine();
+       
+         Console.WriteLine($"your full name is: {LastName}, {fristName} {LastName}.");
     }
 }
