28f1189a0fdd0777971780c9b7d8b598b5b6aecf
D0Flam1ng0 <and24112@byui.edu>
2025-04-29 09:44:53 -0600

    added a comment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,8 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-       
+       // Console.WriteLine("Hello Sandbox World!");
+       int x = 10;
+       if (x==10)
+       {
+        Console.WriteLine("x is 10");
+       }
+       for(int i =0; i<x; i++)
+       {
+            Console.WriteLine($"bob is cool: {i}");
+       }
 
     }
 }
