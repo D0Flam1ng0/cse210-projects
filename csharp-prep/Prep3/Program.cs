e659a6ac27d180bd698491b51d6b990b5c2ab01f
D0Flam1ng0 <and24112@byui.edu>
2025-05-03 10:23:52 -0600

    parts 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       {
+        Random randomNum = new Random();
+        int Answer = randomNum.Next(1, 101);
+
+        int guess = -1;
+
+        
+        while (guess != Answer)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (Answer > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (Answer < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }                    
+    }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,15 +5,18 @@ class Program
     static void Main(string[] args)
     {
        // Console.WriteLine("Hello Sandbox World!");
-       int x = 10;
-       if (x==10)
+       //int x = 10;
+       //if (x==10)
        {
-        Console.WriteLine("x is 10");
+       // Console.WriteLine("x is 10");
        }
-       for(int i =0; i<x; i++)
+       //for(int i =0; i<x; i++)
        {
-            Console.WriteLine($"bob is cool: {i}");
+          //  Console.WriteLine($"bob is cool: {i}");
        }
-
+        int x = 0;
+        int y =x++;
+        Console.WriteLine(x);
+        Console.WriteLine(y);
     }
 }
