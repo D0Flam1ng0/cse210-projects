b41ff885d0fba007f5d1ceef076b09bbf015c2da
D0Flam1ng0 <and24112@byui.edu>
2025-05-06 10:00:10 -0600

    Classes added

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,10 +1,33 @@
 using System;
+using System.Drawing;
 
+class Circle
+{
+    private double _radius;
+   public void SetRaduis(double radius)
+    {
+        if (radius <= 0)
+        {
+            Console.WriteLine("Error");
+            return;
+        }
+        _radius = radius;
+    }
+    public double GetRadius()
+    {
+        return _radius;
+    }
+    public double GetArea()
+    {
+        return Math.PI *_radius *_radius;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-       // Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Bonjour");
+       //Console.WriteLine("Hello Sandbox World!");
        //int x = 10;
        //if (x==10)
        {
@@ -14,9 +37,17 @@ class Program
        {
           //  Console.WriteLine($"bob is cool: {i}");
        }
-        int x = 0;
-        int y =x++;
-        Console.WriteLine(x);
-        Console.WriteLine(y);
+       // int x = 0;
+        //int y =x++;
+        //Console.WriteLine(x);
+        //Console.WriteLine(y);
+        Circle myCircle = new Circle();
+        Circle myCircle2 = new Circle();
+        myCircle.SetRaduis(10);
+        Console.WriteLine($"{myCircle.GetRadius()}");
+        myCircle2.SetRaduis(20);
+        Console.WriteLine($"{myCircle2.GetRadius()}");
+        Console.WriteLine($"{myCircle2.GetArea()}");
     }
+     
 }
