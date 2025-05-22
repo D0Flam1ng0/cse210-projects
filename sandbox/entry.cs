81c52a7854f4ddaad5320faef3cc872255c62568
D0Flam1ng0 <and24112@byui.edu>
2025-05-22 11:29:33 -0600

    frac/script adds

--- sandbox/entry.cs
+++ sandbox/entry.cs
@@ -2,9 +2,9 @@ using System;
 
 public class Entry
 {
-    public string Date { get; set; }
-    public string Prompt { get; set; }
-    public string Response { get; set; }
+    private string Date { get; set; }
+    private string Prompt { get; set; }
+    private string Response { get; set; }
 
     public Entry(string date, string prompt, string response)
     {
---
+++ sandbox/scripture.cs
@@ -0,0 +1,13 @@
+using System;
+
+class scripture
+{
+    Random rnd = new Random();
+    bool running = true;
+    while(running)
+    {
+        Console.WriteLine("Type quit in order to leave or press the enter key in order to contine");
+        
+    }
+}
+// Verse Ideas lets do a doctrinal mastery
